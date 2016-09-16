4e213e8d2a2403e7feb7f0824cfee9d3ccdd6e54
daxiarobin <shenyifeng@gmail.com>
2016-09-16 04:45:40 +0000

    modify client code

--- model/call_pdtconf.py
+++ model/call_pdtconf.py
@@ -148,6 +148,7 @@ def main(argv):
 		DestIp == "":
 		print "\n"+"Missing mandatory arguements"+"\n"
 		parser.print_help()
+		sys.exit()
 	
 	conf = Pdtconf(ConfType,RouterId,Username,Password,RouterPort,
 			AccessProtocol,PolicyName,PolicyVersion,Description,Comment,Identifier,
